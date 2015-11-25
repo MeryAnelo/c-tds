74d438d812b6f52f083d4e489a60a8ceaabbdf9b
Mery Anelo <anelomery@gmail.com>
2015-11-25 17:36:15 -0300

    test... push funciona, problemas -= (creo)

--- test/Test.s
+++ test/Test.s
@@ -1,73 +1,50 @@
 Externprint_int:
-	.globl	fib
-	.type	fib, @function
-fib:
-	movl $0, %eax 
-	movl %eax, -4(%ebp)
-	movl $1, %eax 
-	movl %eax, -8(%ebp)
-	movl 0(%ebp), %eax
-	pushl %eax
-	call print_int
-	movl %eax,-16(%ebp)
-	addl $4, %esp
-while1:
-	movl $0, %eax
-	cmpl -4(%ebp), %eax
-jg isGtr0
-	movl $0, -20(%ebp)
-jmp endGtr0
-isGtr0:
-	movl $1, -20(%ebp)
-endGtr0:
-	movl -20(%ebp), %eax
-	cmpl $0, %eax
-	je end_while3
-	movl -4(%ebp), %eax
-	movl -8(%ebp), %edx
-	addl %edx, %eax
-	movl %eax, -24(%ebp)
-	movl -24(%ebp), %eax 
-	movl %eax, -28(%ebp)
-	movl -8(%ebp), %eax 
-	movl %eax, -4(%ebp)
-	movl -28(%ebp), %eax 
-	movl %eax, -8(%ebp)
-	movl -32(%ebp), %eax
-	movl $1, %edx
-	subl %edx, %eax
-	movl %ecx, %eax
-	movl %eax, -32(%ebp)
-	movl -32(%ebp), %eax
-	pushl %eax
-	call print_int
-	movl %eax,-36(%ebp)
-	addl $4, %esp
-	jmp while1
-end_while3:
-	movl 0(%ebp), %eax
-	leave
-	ret
-end_Methodfib:
-
-	.size fib, .-fib
 	.globl	main
 	.type	main, @function
 main:
 	pushl %ebp
 	movl %esp, %ebp
 	subl $16, %esp
-	movl $6, %eax
-	pushl %eax
-	call fib
-	movl %eax,-44(%ebp)
-	addl $4, %esp
-	movl -44(%ebp), %eax 
-	movl %eax, -48(%ebp)
-	movl -48(%ebp), %eax
-	pushl %eax
+	movl $6, %eax 
+	movl %eax, -4(%ebp)
+	movl $0, %eax 
+	movl %eax, -8(%ebp)
+	movl $1, %eax 
+	movl %eax, -12(%ebp)
+while0:
+	movl $0, %eax
+	cmpl -4(%ebp), %eax
+jg isGtr0
+	movl $0, -16(%ebp)
+jmp endGtr0
+isGtr0:
+	movl $1, -16(%ebp)
+endGtr0:
+	movl -16(%ebp), %eax
+	cmpl $0, %eax
+	je end_while2
+	movl -8(%ebp), %eax
+	movl -12(%ebp), %edx
+	addl %edx, %eax
+	movl %eax, -20(%ebp)
+	movl -20(%ebp), %eax 
+	movl %eax, -24(%ebp)
+	movl -12(%ebp), %eax 
+	movl %eax, -8(%ebp)
+	movl -24(%ebp), %eax 
+	movl %eax, -12(%ebp)
+	movl -4(%ebp), %eax
+	movl $1, %edx
+	subl %edx, %eax
+	movl %ecx, %eax
+	movl %eax, -4(%ebp)
+	jmp while0
+end_while2:
+	movl $3, %eax 
+	movl %eax, -8(%ebp)
+	pushl -8(%ebp)
 	call print_int
-	movl %eax,-52(%ebp)
+	movl %eax,-28(%ebp)
 	addl $4, %esp
 	nop
 	leave
