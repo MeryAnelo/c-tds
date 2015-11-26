249470b88fa4e07cbf2a6170f1ca3d818f24a7a1
Mery Anelo <anelomery@gmail.com>
2015-11-25 21:05:20 -0300

    Comentarios colocados en VisitorCI.java y Test.compi

--- test/Test.s
+++ test/Test.s
@@ -4,47 +4,46 @@ Externprint_int:
 main:
 	pushl %ebp
 	movl %esp, %ebp
-	subl $16, %esp
-	movl $6, %eax 
+	subl $--4, %esp
+	movl $100, %eax 
 	movl %eax, -4(%ebp)
-	movl $0, %eax 
+	movl $6, %eax 
 	movl %eax, -8(%ebp)
-	movl $1, %eax 
+	movl $0, %eax 
 	movl %eax, -12(%ebp)
+	movl $1, %eax 
+	movl %eax, -16(%ebp)
 while0:
-	movl $0, %eax
-	cmpl -4(%ebp), %eax
+	movl -8(%ebp), %eax
+	cmpl $0, %eax
 jg isGtr0
-	movl $0, -16(%ebp)
+	movl $0, -20(%ebp)
 jmp endGtr0
 isGtr0:
-	movl $1, -16(%ebp)
+	movl $1, -20(%ebp)
 endGtr0:
-	movl -16(%ebp), %eax
+	movl -20(%ebp), %eax
 	cmpl $0, %eax
 	je end_while2
-	movl -8(%ebp), %eax
-	movl -12(%ebp), %edx
+	movl -12(%ebp), %eax
+	movl -16(%ebp), %edx
 	addl %edx, %eax
-	movl %eax, -20(%ebp)
-	movl -20(%ebp), %eax 
 	movl %eax, -24(%ebp)
-	movl -12(%ebp), %eax 
-	movl %eax, -8(%ebp)
 	movl -24(%ebp), %eax 
+	movl %eax, -28(%ebp)
+	movl -16(%ebp), %eax 
 	movl %eax, -12(%ebp)
-	movl -4(%ebp), %eax
+	movl -28(%ebp), %eax 
+	movl %eax, -16(%ebp)
+	movl -8(%ebp), %eax
 	movl $1, %edx
 	subl %edx, %eax
-	movl %ecx, %eax
-	movl %eax, -4(%ebp)
+	movl %eax, -8(%ebp)
 	jmp while0
 end_while2:
-	movl $3, %eax 
-	movl %eax, -8(%ebp)
-	pushl -8(%ebp)
+	pushl -12(%ebp)
 	call print_int
-	movl %eax,-28(%ebp)
+	movl %eax,-32(%ebp)
 	addl $4, %esp
 	nop
 	leave
